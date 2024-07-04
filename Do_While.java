5c71a541c67112e80f7e4d4a0b4daa8408269fa6
Amit Singh <129637346+amitsingh256@users.noreply.github.com>
2024-07-04 22:16:18 +0530

    Add files via upload

---
+++ Do_While.java
@@ -0,0 +1,35 @@
+package Java_Examples;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+import javax.sound.midi.Soundbank;
+
+public class Do_While {
+	public static void main(String[] args) {
+		System.out.println("");
+		Scanner in = new Scanner(System.in);
+		 
+		//System.out.println("first 10 natural number");
+	  
+		//int i=10;
+		/*System.out.println("first 10 natural number");
+		int num=in.nextInt(); 
+		
+		while(i<=10) {
+		System.out.println(i*num);
+    	i++;   
+		 }  
+		
+		do {
+			System.out.println(i);
+			i++;
+		} while (i<=10);
+		*/
+		int i;
+	for(i=7;i>=0;i--) {
+		System.out.println(i);
+	}
+		}      
+		}
+
